a69f610771f71ba34a42fa107d6f0d2b1cc40598
kaylanhua <kaylahuang@college.harvard.edu>
2024-06-18 17:01:42 -0400

    added war var to views rf

--- components/views_cleaner.py
+++ components/views_cleaner.py
@@ -8,6 +8,7 @@ from datetime import datetime
 class VIEWSCleaner():
     def __init__(self, filename, gw_id):
         self.features = self.load_data(filename, gw_id)
+        self.features_war_dates = None
         self.gw_id = gw_id
 
     def load_data(self, filename, gw_id):
@@ -32,5 +33,30 @@ class VIEWSCleaner():
         plt.gcf().set_facecolor('#F9F5F1')
 
         plt.title(f'Fatalities Over Time (views {self.gw_id})')
-        plt.legend()
-        plt.show
+        plt.legend(['Ground Truth'])
+        plt.show()
+        
+    def set_war_var(self, data, date_ranges):
+        # data = self.features
+        
+        def calculate_month_id(date):
+            return 121 + (date.year - 1990) * 12 + (date.month - 1)
+
+        int_dates = [(calculate_month_id(date), calculate_month_id(end)) for date, end in date_ranges]
+        self.war_dates = int_dates
+        print(self.war_dates)
+        
+        def populate_war_vars(date):
+            months_since_start = np.nan
+            for start, end in int_dates:
+                if start <= date:
+                    months_since_start = date - start
+                if start <= date <= end:
+                    return 1, months_since_start
+            return 0, months_since_start
+
+        # Apply the function to the 'date_start' column in the data
+        data['wartime'], data['since_war_start'] = zip(*data['month_id'].apply(populate_war_vars))
+        
+        self.features_war_dates = data
+        return data
