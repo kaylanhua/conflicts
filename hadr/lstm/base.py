67bd818fd4998e2a53603f47b52f292418f75fb4
kaylanhua <kaylahuang@college.harvard.edu>
2024-08-13 11:50:16 -0400

    multi runs finally

--- hadr/lstm/base.py
+++ hadr/lstm/base.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+
 
 # DATA SECTION ----------------------------------------------------------------
 def create_sequences(data, seq_length):
@@ -19,33 +21,67 @@ def create_sequences(data, seq_length):
         
     return np.array(xs), np.array(ys)
 
+def create_multivariate_sequences(input_sequences, output_sequence, n_steps_in, n_steps_out):
+    X, y = list(), list() # instantiate X and y
+    for i in range(len(input_sequences)):
+        # find the end of the input, output sequence
+        end_ix = i + n_steps_in
+        out_end_ix = end_ix + n_steps_out - 1
+        # check if we are beyond the dataset
+        if out_end_ix > len(input_sequences): break
+        
+        # gather input and output of the pattern
+        seq_x, seq_y = input_sequences[i:end_ix], output_sequence[end_ix-1:out_end_ix, -1]
+        X.append(seq_x), y.append(seq_y)
+    
+    return np.array(X), np.array(y)
+
 country = 'drc'
 # types: 'attention', 'base', 'multi'
-lstm_type = 'base'
+lstm_type = 'multi'
 # all_history = pd.read_csv('../../data/views/drc_no_rolling.csv')
-all_history = pd.read_csv('drc_no_rolling.csv').dropna()
+all_history = pd.read_csv('drc_features.csv').dropna()
 # all_history = all_history.head(140)
-history = all_history['ged_sb'].tolist()
-# history = all_history['rolling_avg'].tolist()
 
 # NORMALIZATION SECTION 
-# # Perform Log Transformation on history
-# history = [np.log1p(x) for x in history]  # Using log1p to handle zero values
+if lstm_type == 'multi':
+    X, y = all_history.drop(columns=['ged_sb']), all_history.ged_sb.values
+    mm = MinMaxScaler() # can also use the one below that univariate models use
+    ss = StandardScaler()
+    X_trans = ss.fit_transform(X)
+    y_trans = mm.fit_transform(y.reshape(-1, 1))
+    
+    X_ss, y_mm = create_multivariate_sequences(X_trans, y_trans, 12, 6) # tweak these hyperparameters
+    print(f"Multivariate data shapes for X, y: {X_ss.shape}, y: {y_mm.shape}")
+    
+    total_samples = len(X)
+    train_test_cutoff = round(0.90 * total_samples)
+    X_train, X_test = X_ss[:train_test_cutoff], X_ss[train_test_cutoff:]
+    y_train, y_test = y_mm[:train_test_cutoff], y_mm[train_test_cutoff:]
+    
+    print(f"X train and test shapes: {X_train.shape}, {X_test.shape}")
+    print(f"y train and test shapes: {y_train.shape}, {y_test.shape}")
+    
+else:
+    history = all_history['ged_sb'].tolist()
+    
+    # Perform Log Transformation on history
+    # history = [np.log1p(x) for x in history]  # Using log1p to handle zero values
 
-# Perform Min-Max Normalization on history
-min_val = min(history)
-max_val = max(history)
-history_normalized = [(x - min_val) / (max_val - min_val) for x in history]
-history = history_normalized
-print(f"Normalized data - Min: {min(history):.4f}, Max: {max(history):.4f}")
+    # Perform Min-Max Normalization on history
+    min_val = min(history)
+    max_val = max(history)
+    history_normalized = [(x - min_val) / (max_val - min_val) for x in history]
+    history = history_normalized
+    print(f"Normalized data - Min: {min(history):.4f}, Max: {max(history):.4f}")
 
-seq_length = 5
-X, y = create_sequences(history, seq_length)
+    seq_length = 5
+    X, y = create_sequences(history, seq_length)
 
-# split train and test
-train_size = int(len(y) * 0.8)
-X_train, X_test = X[:train_size], X[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
+    # split train and test
+    train_size = int(len(y) * 0.8)
+    X_train, X_test = X[:train_size], X[train_size:]
+    y_train, y_test = y[:train_size], y[train_size:]
 
 X_train = torch.from_numpy(X_train).float()
 y_train = torch.from_numpy(y_train).float()
@@ -123,9 +159,11 @@ class LSTMMultivariate(nn.Module):
         h0 = torch.zeros(self.num_layers, B, self.hidden_size).to(x.device)
         c0 = torch.zeros(self.num_layers, B, self.hidden_size).to(x.device)
         
-        out, (hn, cn) = self.lstm(x, (h0, c0))
-        hn = hn.view(-1, self.hidden_size) # reshaping the data for Dense layer next
-        out = self.relu(hn)
+        output, (hn, cn) = self.lstm(x, (h0, c0))
+        out = output[:, -1, :]
+        # hn = hn.view(-1, self.hidden_size) # reshaping the data for Dense layer next
+        
+        out = self.relu(out)
         out = self.fc1(out)
         out = self.relu(out)
         out = self.fc2(out)
@@ -133,18 +171,19 @@ class LSTMMultivariate(nn.Module):
         
     
 # INITIALIZING THE MODEL -----------------------------------------------------------
-input_size = 1 # univariate
+input_size = 4 # univariate
 hidden_size = 50
 num_layers = 3
-output_size = 1
+output_size = 6
 
 if lstm_type == 'base':
     # LSTM model: hidden size 70, num layers 4, epochs 180, LR 0.01, seq_length 10
     model = LSTM(input_size, hidden_size, num_layers, output_size)
 elif lstm_type == 'attention':
-    # LSTM Attention model: hidden size , num layers 
     model = LSTMAttention(input_size, hidden_size, output_size, num_layers)
-
+elif lstm_type == 'multi':
+    model = LSTMMultivariate(input_size, hidden_size, output_size, num_layers)
+    
 # TRAINING THE MODEL ----------------------------------------------------------------
 learning_rate = 0.01
 num_epochs = 200
@@ -152,40 +191,75 @@ num_epochs = 200
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-model.train()
-for epoch in range(num_epochs):
-    train_outputs = model(X_train.unsqueeze(-1)).squeeze()
-    optimizer.zero_grad()
-    loss = criterion(train_outputs, y_train)
-    loss.backward()
-    optimizer.step()
-    
-    if (epoch + 1) % 10 == 0:
-        print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}")
+if lstm_type != 'multi':
+    model.train()
+    for epoch in range(num_epochs):
+        train_outputs = model(X_train.unsqueeze(-1)).squeeze()
+        optimizer.zero_grad()
+        loss = criterion(train_outputs, y_train)
+        loss.backward()
+        optimizer.step()
         
-print("!! Training complete !!")
+        if (epoch + 1) % 10 == 0:
+            print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {loss.item():.4f}")
+            
+    print("!! Training complete !!")
+
+def multivariate_training_loop(n_epochs, lstm, optimizer, loss_fn, X_train, y_train,
+                  X_test, y_test):
+    for epoch in range(n_epochs):
+        lstm.train()
+        outputs = lstm.forward(X_train)
+        optimizer.zero_grad() # calculate the gradient, manually setting to 0
+        loss = loss_fn(outputs, y_train)
+        loss.backward() # calculates the loss of the loss function
+        optimizer.step() # improve from loss, i.e backprop
+        
+        # test loss
+        lstm.eval()
+        test_preds = lstm(X_test)
+        test_loss = loss_fn(test_preds, y_test)
+        
+        if epoch % 10 == 0:
+            print("Epoch: %d, train loss: %1.5f, test loss: %1.5f" % (epoch, 
+                                                                      loss.item(), 
+                                                                      test_loss.item())) 
 
 
 # TESTING THE MODEL ----------------------------------------------------------------
-with torch.no_grad():
-    test_outputs = model(X_test.unsqueeze(-1)).squeeze()
-    test_loss = criterion(test_outputs, y_test)
-    print(f"Test Loss: {test_loss.item():.4f}")
-    print("!! Testing complete !!")
+
+if lstm_type == 'multi':
+    multivariate_training_loop(n_epochs=num_epochs,
+              lstm=model,
+              optimizer=optimizer,
+              loss_fn=criterion,
+              X_train=X_train,
+              y_train=y_train,
+              X_test=X_test,
+              y_test=y_test)
     
+    
+    
+else:
+    with torch.no_grad():
+        test_outputs = model(X_test.unsqueeze(-1)).squeeze()
+        test_loss = criterion(test_outputs, y_test)
+        print(f"Test Loss: {test_loss.item():.4f}")
+        print("!! Testing complete !!")
+        
 
-all_outputs = np.concatenate((train_outputs.detach().numpy(), test_outputs.detach().numpy()))
+    all_outputs = np.concatenate((train_outputs.detach().numpy(), test_outputs.detach().numpy()))
 
-# Calculate the index where the test set starts
-test_start_index = len(history) - len(y_test) - seq_length
+    # Calculate the index where the test set starts
+    test_start_index = len(history) - len(y_test) - seq_length
 
-# Plot the true values and the predictions
-plt.plot(history, label="True Values")
-plt.plot(range(seq_length, seq_length + len(all_outputs)), all_outputs, label="Predictions")
-plt.axvline(x=test_start_index, color='gray', linestyle='--', label="Test set start")
-plt.xlabel("Time")
-plt.ylabel("Value")
-plt.legend()
-plt.title("LSTM Predictions vs True Values")
-plt.savefig(f'{country}_{lstm_type}__EP{num_epochs}__NL{num_layers}__HS{hidden_size}__SL{seq_length}.jpg')
-plt.close()
+    # Plot the true values and the predictions
+    plt.plot(history, label="True Values")
+    plt.plot(range(seq_length, seq_length + len(all_outputs)), all_outputs, label="Predictions")
+    plt.axvline(x=test_start_index, color='gray', linestyle='--', label="Test set start")
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.legend()
+    plt.title("LSTM Predictions vs True Values")
+    plt.savefig(f'{country}_{lstm_type}__EP{num_epochs}__NL{num_layers}__HS{hidden_size}__SL{seq_length}.jpg')
+    plt.close()
--- hadr/notes.md
+++ hadr/notes.md
@@ -10,6 +10,7 @@ v1: results from zero shot prompting
 RAG: rag attempt with GDELT information 
 LSTM: baseline lstm from [this tutorial](https://medium.com/@mike.roweprediger/using-pytorch-to-train-an-lstm-forecasting-model-e5a04b6e0e67)
 - multivariate can use this [tutorial](https://charlieoneill.medium.com/predicting-the-price-of-bitcoin-with-multivariate-pytorch-lstms-695bc294130)
+- attention and LSTM can use this [tutorial](https://medium.com/@aidant0001/revolutionizing-time-series-prediction-with-lstm-with-the-attention-mechanism-2bd126e9fdf1)
 
 ## folder paths
 hadr 
