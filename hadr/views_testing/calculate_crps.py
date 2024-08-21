324c76c548f1e592896b155e02e9180b8d340ce8
kaylanhua <kaylahuang@college.harvard.edu>
2024-08-21 17:07:51 -0500

    got the colors right

--- hadr/views_testing/calculate_crps.py
+++ hadr/views_testing/calculate_crps.py
@@ -15,7 +15,7 @@ def calculate_crps(actuals_file, forecasts_file):
     else:
         # Handle LSTM format
         # forecast_columns = [col for col in forecasts_df.columns if col.startswith('forecast_')]
-        forecast_columns = ['forecast_1', 'forecast_3']
+        forecast_columns = ['forecast_3', 'forecast_1']
         # sets negative
         forecasts = forecasts_df.groupby('month_id')[forecast_columns].apply(lambda x: np.maximum(x.values.flatten(), 0).tolist()).reset_index(name='forecasts')
     
@@ -44,5 +44,10 @@ if __name__ == "__main__":
     
     print("Comparison of Conflictology and LSTM Results:")
     for i in range(len(conflictology_results)):
-        print(f"\033[1m--------------------------------\nmonth_id: {conflictology_results[i]['month_id']}, observation: {conflictology_results[i]['observation']}\nConflictology forecast: {conflictology_results[i]['forecast']}, CRPS: {conflictology_results[i]['crps_score']:.4f}\nLSTM forecast: {lstm_results[i]['forecast']}, CRPS: {lstm_results[i]['crps_score']:.4f}\033[0m")
-        print("\033[91m" if conflictology_results[i]['crps_score'] > lstm_results[i]['crps_score'] else "\033[92m", end="")
+        conflictology_crps = conflictology_results[i]['crps_score']
+        lstm_crps = lstm_results[i]['crps_score']
+        color = "\033[91m" if lstm_crps > conflictology_crps else "\033[92m"
+        print(f"{color}--------------------------------")
+        print(f"month_id: {conflictology_results[i]['month_id']}, observation: {conflictology_results[i]['observation']}")
+        print(f"Conflictology forecast: {conflictology_results[i]['forecast']}, CRPS: {conflictology_crps:.4f}")
+        print(f"LSTM forecast: {lstm_results[i]['forecast']}, CRPS: {lstm_crps:.4f}\033[0m")
